73386151b4fc2d151f321811abf2355b20fb7361
Vera <solnce288888@gmail.com>
2022-09-28 22:40:49 +0300

    example005 hello for masha

---
+++ example005_Hello_espesial/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter the user name: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
